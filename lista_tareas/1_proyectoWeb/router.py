6d9a3934381c6c80ac5151e0403ad1eec35469f3
Juan David Gomez Lopez <72229962+juandagomezl@users.noreply.github.com>
2021-02-22 17:59:18 -0500

    ..

--- lista_tareas/1_proyectoWeb/router.py
+++
@@ -1,8 +0,0 @@
-from data_Api.viewsets import Datos_clientesViewset
-from rest_framework import routers
-
-
-router = routers.DefaultRouter()
-router.register('datos_clientes', Datos_clientesViewset)
-
-
